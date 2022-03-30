7890b66e7032d4c1879147da06c9cff311686bd9
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-03-30 20:57:48 +0300

    lesson 6 task 1

---
+++ lesson-6/task-1/webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: 'images',
+              limit: 1000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
