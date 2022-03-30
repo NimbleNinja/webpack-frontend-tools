1a6dc88d4f903b6fd35fb78ad46567346c0d0cd2
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-03-30 21:05:12 +0300

    lesson 6 task 1 fix 8kb

--- lesson-6/task-1/webpack.config.js
+++ lesson-6/task-1/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             options: {
               name: '[path][name].[ext]',
               outputPath: 'images',
-              limit: 1000,
+              limit: 8000,
             },
           },
         ],
